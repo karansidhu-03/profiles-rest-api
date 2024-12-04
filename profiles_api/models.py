be06525ac7d82e6ebb93febf41aa013df95786e5
Karanvir Sidhu <karanvirsidhu03@gmail.com>
2024-12-03 19:13:51 -0700

    Added Login API

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -20,7 +20,6 @@ class UserProfileManager(BaseUserManager):
 
     def create_superuser(self, email, name, password):
         user = self.create_user(email, name, password)
-
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from profiles_api import views
 from rest_framework import routers
 
+
 router = routers.DefaultRouter()
-router.register("viewset", views.HelloViewSet, basename="viewset")
+router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
 router.register("profile", views.ProfileViewSet)
 
 urlpatterns = [
     path("hello-view/", views.HelloAPIView.as_view()),
+    path("login/", views.UserLoginApiView.as_view()),
     path("", include(router.urls)),
 ]
